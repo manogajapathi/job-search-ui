ba26e0bacac19e26b71b9c442913c862cf7ae58f
Manogajapathi <37205789+manogajapathi@users.noreply.github.com>
2018-07-04 02:32:21 +0530

    logo fixed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <div className="logo-style">
-            <img className="logo" src="/src/imgs/logo.png" />
+            <img className="logo" src="./src/imgs/logo.png" />
           </div>
         </header>
         <content>
